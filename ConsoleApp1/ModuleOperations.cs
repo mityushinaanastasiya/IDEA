04daee07ab0a2bfac38a512ed3eb08c66148bbf6
Анастасия Митюшина <nastya-mityushina@mail.ru>
2019-02-28 19:16:37 +0300

    Небольшой код ревью

---
+++ ConsoleApp1/ModuleOperations.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ModuleOperations
+    {
+        /// <summary>
+        /// побитовое исключающее или
+        /// </summary>
+        /// <param name="A">Первый член</param>
+        /// <param name="B">Второй член</param>
+        /// <returns></returns>
+        public ushort XOR(ushort A, ushort B)
+        {
+            uint result32;
+            uint A32 = Convert.ToUInt32(A);
+            uint B32 = Convert.ToUInt32(B);
+            result32 = A32 ^ B32;
+            return Convert.ToUInt16(result32);
+        }
+        /// <summary>
+        /// Умножение по модулю 2^16 + 1 то есть 65 537
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <returns></returns>
+        public ushort multiplication(ushort A, ushort B)
+        {
+            if (A == 0 && B == 0) return 0;
+            ulong Aulong = (A == 0) ? 65536 : Convert.ToUInt64(A);
+            ulong Bulong = (B == 0) ? 65536 : Convert.ToUInt64(B);
+            ulong mult = (Aulong * Bulong) % 65537;
+            if (mult == 65536) return 0;
+            return Convert.ToUInt16(mult);
+        }
+
+        /// <summary>
+        /// Мультипликативная инверсия по модулю 2^16 + 1 
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public ushort multiplicationInversion(ushort A)
+        {
+            long a = 65537, b = A, x1 = 1, x2 = 0, x;
+            while (b != 1)
+            {
+                x = x2 - ((a / b) * x1);
+                x2 = x1;
+                x1 = x;
+                x = a % b;
+                a = b;
+                b = x;
+            }
+            long res = (x1 + 65537) % 65537;
+            int prov = multiplication(Convert.ToUInt16(res), A) % 65537; //проверка
+            return Convert.ToUInt16(res);
+        }
+        /// <summary>
+        /// Сложение по модулю 2^16 то есть 65 536
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <returns></returns>
+
+        public ushort addition(ushort A, ushort B)
+        {
+            uint summ = Convert.ToUInt32(A + B);
+            ushort result = Convert.ToUInt16(summ % 65536);
+            return result;
+        }
+
+        /// <summary>
+        /// Аддитивная инверсия
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public ushort additiveInversion(ushort A)
+        {
+            if (A == 0) return 0;
+            else return Convert.ToUInt16(65536 - A);
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,16 @@ namespace ConsoleApp1
 {
     class Program
     {
+        //исходный ключ, который будет разбиваться 
         static BitArray key = new BitArray(128);
+        //подключи для шифрования
         static List<ushort> subKeys = new List<ushort>();
+        //подключи для дешифрования
         static List<ushort> subKeysDeshifr = new List<ushort>();
+
+        //класс работы с операциями 
+        static ModuleOperations moduleOperations = new ModuleOperations();
+        //побитовый сдвиг на 25 бит  влево
         static void shiftKeyToLeft25(BitArray key)
         {
             bool buf;
@@ -26,16 +33,27 @@ namespace ConsoleApp1
                 key[127] = buf;
             }
         }
+
+        /// <summary>
+        /// Генерация ключей для шифрования
+        /// </summary>
         static void generationKeys()
         {
-            ////случайная генерация ключа
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //НИЖЕ закоментирован код генерации ключа и субключа
+            // ДЛЯ ОТЛАДКИ ИСПОЛЬЗУЮ СТАТИЧЕСКИЙ НАБОР СУБКЛЮЧЕЙ
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            ////случайная генерация мсхдного ключа
             //Random random = new Random();
             //for (int i = 0; i < 128; i++)
             //{
             //    key[i] = random.Next(2) == 0 ? true : false;
             //}
+
             ////разбиваем 128-битный ключ на восемь 16-битных блоков
-            //for (int j = 0; j < 8; j++) 
+            ////128-битовый ключ IDEA определяет первые восемь субключей
+            //for (int j = 0; j < 8; j++)
             //{
             //    BitArray oneBlock = new BitArray(16);
             //    //считываем 16 бит в один блок
@@ -45,8 +63,11 @@ namespace ConsoleApp1
             //    }
             //    int[] buf = new int[1];
             //    oneBlock.CopyTo(buf, 0); // получила в инт 32 
-            //    subKeys.Add(Convert.ToUInt16(buf[0])); 
+            //    subKeys.Add(Convert.ToUInt16(buf[0]));
             //}
+
+            ////Последующие ключи (44) получаются путем последовательности 
+            ///цик-лических сдвигов влево этого кода на 25 двоичных разрядов.
             ////пока не 52 ключа выполняй
             //while (subKeys.Count != 52)
             //{
@@ -69,110 +90,65 @@ namespace ConsoleApp1
             //    }
             //}
 
-            while (subKeys.Count != 52)
+            //тестовая версия из его документика
+            //страница 65
+            ushort[] k = new ushort[52] { 55, 54, 53, 52, 51, 50, 49, 56,
+                27648, 27136, 26624, 26112, 25600, 25088, 28672, 28160,
+                208, 204, 200, 196, 224, 220, 216, 212, 38913, 36865,
+                34817, 49153, 47105, 45057, 43009, 40961, 784, 896,
+                880, 864, 848, 832, 816, 800, 6, 57350, 49158,
+                40966, 32774, 24582, 16390, 8199, 3456, 3392, 3328, 3328 };
+
+            for (int i = 0; i < 52; i++)
             {
-                subKeys.Add(18);
+                subKeys.Add(k[i]);
             }
+
         }
+
+        /// <summary>
+        /// Генерация ключей для дешифрования
+        /// </summary>
         static void generationDeKeys()
         {
-            subKeysDeshifr.Add(multiplicationInversion(subKeys[48]));
-            subKeysDeshifr.Add(additiveInversion(subKeys[49]));
-            subKeysDeshifr.Add(additiveInversion(subKeys[50]));
-            subKeysDeshifr.Add(multiplicationInversion(subKeys[51]));
+            //Первые четыре субключа расшифрования определяются несколько иначе, чем остальные.
+            subKeysDeshifr.Add(moduleOperations.multiplicationInversion(subKeys[48]));
+            subKeysDeshifr.Add(moduleOperations.additiveInversion(subKeys[49]));
+            subKeysDeshifr.Add(moduleOperations.additiveInversion(subKeys[50]));
+            subKeysDeshifr.Add(moduleOperations.multiplicationInversion(subKeys[51]));
 
+
+            //Последующие операции производятся восемь раз 
+            //с добавлением 6 к индексу ключей расшифрования 
+            //и вычитанием 6 из индекса ключей шифрования
             for (int i = 0; i < 8; i++)
             {
                 subKeysDeshifr.Add(subKeys[46 - i * 6]);
                 subKeysDeshifr.Add(subKeys[47 - i * 6]);
-                subKeysDeshifr.Add(multiplicationInversion(subKeys[42 - i * 6]));
-                subKeysDeshifr.Add(additiveInversion(subKeys[44 - i * 6]));
-                subKeysDeshifr.Add(additiveInversion(subKeys[43 - i * 6]));
-                subKeysDeshifr.Add(multiplicationInversion(subKeys[45 - i * 6]));
+                subKeysDeshifr.Add(moduleOperations.multiplicationInversion(subKeys[42 - i * 6]));
+                subKeysDeshifr.Add(moduleOperations.additiveInversion(subKeys[44 - i * 6]));
+                subKeysDeshifr.Add(moduleOperations.additiveInversion(subKeys[43 - i * 6]));
+                subKeysDeshifr.Add(moduleOperations.multiplicationInversion(subKeys[45 - i * 6]));
             }
         }
 
-        //побитовое исключающее или
-        static ushort XOR(ushort A, ushort B)
-        {
-            uint result32;
-            uint A32 = Convert.ToUInt32(A);
-            uint B32 = Convert.ToUInt32(B);
-            result32 = A32 ^ B32;
-            return Convert.ToUInt16(result32);
-        }
-        //Умножение по модулю 2^16 + 1 то есть 65 537
-        static ushort multiplication(ushort A, ushort B)
-        {
-            if (A == 0 && B == 0) return 0;
-            ulong Aulong = (A == 0) ? 65536 : Convert.ToUInt64(A);
-            ulong Bulong = (B == 0) ? 65536 : Convert.ToUInt64(B);
-            ulong mult = (Aulong * Bulong) % 65537;
-            if (mult == 65536) return 0;
-            return Convert.ToUInt16(mult);
-        }
-        //Мультипликативная инверсия по модулю 2^16 + 1 
-        static ushort multiplicationInversion(ushort A)
-        {
-            //uint q, r, t, r1 = 65537, r2 = A, t1 = 0, t2 = 1;
-            //    uint res = 0;
-            //    while (r2 > 0)
-            //    {
-            //        q = r1 / r2;
 
-
-            //        r = r1 - q * r2;
-            //        r1 = r2;
-            //        r2 = r;
-
-            //        t = t1 - q * t2;
-            //        t1 = t2;
-            //        t2 = r;
-            //    }
-            //    if (r1 == 1) res = t1;
-            //    uint prov = (res * A) % 65537;
-            //}
-
-            uint a = 65537, b = A, x1 = 1, x2 = 0, x;
-            while (b != 1)
-            {
-                x = x2 - ((a / b) * x1);
-                x2 = x1;
-                x1 = x;
-                x = a % b;
-                a = b;
-                b = x;
-            }
-            uint res = x1 % 65537;
-
-            uint prov = (res * A) % 65537;
-            return Convert.ToUInt16(res);
-        }
-        //Аддитивная инверсия
-        static ushort additiveInversion(ushort A)
-        {
-            if (A == 0) return 0;
-            else return Convert.ToUInt16(65536 - A);
-        }
-
-        //Сложение по модулю 2^16 то есть 65 536
-        static ushort addition(ushort A, ushort B)
-        {
-            uint summ = Convert.ToUInt32(A + B);
-            ushort result = Convert.ToUInt16(summ % 65536);
-            return result;
-        }
-
-        //шифратор
+        /// <summary>
+        /// Шифратор
+        /// </summary>
         static void encryptingIDEA()
         {
 
-            //добавление 
+            
             string textHP = @".\HP.txt"; ;
             FileStream fstream = new FileStream(textHP, FileMode.Open);
+
             //проверка на кол-во байт 
-            int remainder = Convert.ToInt32(fstream.Length % 8); // остаток байтов 
-            int additionaByte=0; //добавлено байтов в конце
+            //необходимо чтобы входящий поток байтов (lenght)
+            //был кратен 64
+            int remainder = Convert.ToInt32(fstream.Length % 8); // остаток от деления  
+            int additionaByte=0; //сколько байтов будет добавлено в конец файла
+            //если у нас есть остаток, то дописываем байты необходимые для кратности 
             if (remainder != 0)
             {
                 fstream.Position = fstream.Length;
@@ -182,6 +158,7 @@ namespace ConsoleApp1
                     fstream.WriteByte(10);
                 }
             }
+            //встаем в начало файла
             fstream.Position = 0;
 
             FileStream FSWrite = new FileStream(@".\code.IDE", FileMode.Create);
@@ -202,27 +179,30 @@ namespace ConsoleApp1
                 ushort C = Convert.ToUInt16((I & 0x00000000FFFF0000) >> 16);
                 ushort D = Convert.ToUInt16(I & 0x000000000000FFFF);
 
+
+                // применяем к считанным блокам шифрацию 
+                // согласно его примеру 
                 ushort T1, T2;
                 for (int i = 0; i < 8; i++)
                 {
-                    A = multiplication(A, subKeys[i * 6]);
-                    B = addition(B, subKeys[i * 6 + 1]);
-                    C = addition(C, subKeys[i * 6 + 2]);
-                    D = multiplication(D, subKeys[i * 6 + 3]);
+                    A = moduleOperations.multiplication(A, subKeys[i * 6]);
+                    B = moduleOperations.addition(B, subKeys[i * 6 + 1]);
+                    C = moduleOperations.addition(C, subKeys[i * 6 + 2]);
+                    D = moduleOperations.multiplication(D, subKeys[i * 6 + 3]);
 
-                    T1 = XOR(A, C);
-                    T2 = XOR(B, D);
-                    T1 = multiplication(T1, subKeys[i * 6 + 4]);
-                    T2 = addition(T1, T2);
-                    T2 = multiplication(T2, subKeys[i * 6 + 5]);
-                    T1 = addition(T1, T2);
+                    T1 = moduleOperations.XOR(A, C);
+                    T2 = moduleOperations.XOR(B, D);
+                    T1 = moduleOperations.multiplication(T1, subKeys[i * 6 + 4]);
+                    T2 = moduleOperations.addition(T1, T2);
+                    T2 = moduleOperations.multiplication(T2, subKeys[i * 6 + 5]);
+                    T1 = moduleOperations.addition(T1, T2);
 
-                    A = XOR(A, T2);
-                    B = XOR(B, T1);
-                    C = XOR(C, T2);
-                    D = XOR(D, T1);
+                    A = moduleOperations.XOR(A, T2);
+                    B = moduleOperations.XOR(B, T1);
+                    C = moduleOperations.XOR(C, T2);
+                    D = moduleOperations.XOR(D, T1);
 
-                    //смена местамми В и С 
+                    //смена местамми В и С во всех раундах кроме псоледнего
                     if (i != 7)
                     {
                         ushort buf = B;
@@ -232,11 +212,13 @@ namespace ConsoleApp1
                 }
 
                 //дополнительные преобразования, 9ый раунд алгоритма
-                A = multiplication(A, subKeys[48]);
-                B = addition(B, subKeys[49]);
-                C = addition(C, subKeys[50]);
-                D = multiplication(D, subKeys[51]);
+                A = moduleOperations.multiplication(A, subKeys[48]);
+                B = moduleOperations.addition(B, subKeys[49]);
+                C = moduleOperations.addition(C, subKeys[50]);
+                D = moduleOperations.multiplication(D, subKeys[51]);
 
+
+                //запись в файл 
                 byte[] result = new byte[8];
                 result[0] = Convert.ToByte((A & 0xFF00) >> 8);
                 result[1] = Convert.ToByte(A & 0x00FF);
@@ -254,20 +236,25 @@ namespace ConsoleApp1
             fstream.Close();
         }
 
-        //дешифратор
+
+        /// <summary>
+        /// Дешифратор
+        /// </summary>
         static void decryptingIDEA()
         {
-            //добавление 
+
             string codeIDE = @".\code.IDE"; ;
             FileStream fstream = new FileStream(codeIDE, FileMode.Open);
 
-            //считываем сколько байтов надо будет потом удалить
+            //считываем из конца файла, сколько байтов надо будет потом удалить
             fstream.Position  = fstream.Length - 1;
             int countDeletByte = fstream.ReadByte();
             
+            // в начало файла
             fstream.Position = 0;
 
             FileStream FSWrite = new FileStream(@".\decode1.IDE", FileMode.Create);
+
             //считывание из файла по 64 бита и дробление на блоки по 16 бит 
             while (fstream.Position != fstream.Length-1)
             {
@@ -288,22 +275,22 @@ namespace ConsoleApp1
                 ushort T1, T2;
                 for (int i = 0; i < 8; i++)
                 {
-                    A = multiplication(A, subKeysDeshifr[i * 6]);
-                    B = addition(B, subKeysDeshifr[i * 6 + 1]);
-                    C = addition(C, subKeysDeshifr[i * 6 + 2]);
-                    D = multiplication(D, subKeysDeshifr[i * 6 + 3]);
+                    A = moduleOperations.multiplication(A, subKeysDeshifr[i * 6]);
+                    B = moduleOperations.addition(B, subKeysDeshifr[i * 6 + 1]);
+                    C = moduleOperations.addition(C, subKeysDeshifr[i * 6 + 2]);
+                    D = moduleOperations.multiplication(D, subKeysDeshifr[i * 6 + 3]);
 
-                    T1 = XOR(A, C);
-                    T2 = XOR(B, D);
-                    T1 = multiplication(T1, subKeysDeshifr[i * 6 + 4]);
-                    T2 = addition(T1, T2);
-                    T2 = multiplication(T2, subKeysDeshifr[i * 6 + 5]);
-                    T1 = addition(T1, T2);
+                    T1 = moduleOperations.XOR(A, C);
+                    T2 = moduleOperations.XOR(B, D);
+                    T1 = moduleOperations.multiplication(T1, subKeysDeshifr[i * 6 + 4]);
+                    T2 = moduleOperations.addition(T1, T2);
+                    T2 = moduleOperations.multiplication(T2, subKeysDeshifr[i * 6 + 5]);
+                    T1 = moduleOperations.addition(T1, T2);
 
-                    A = XOR(A, T2);
-                    B = XOR(B, T1);
-                    C = XOR(C, T2);
-                    D = XOR(D, T1);
+                    A = moduleOperations.XOR(A, T2);
+                    B = moduleOperations.XOR(B, T1);
+                    C = moduleOperations.XOR(C, T2);
+                    D = moduleOperations.XOR(D, T1);
 
                     //смена местамми В и С 
                     if (i != 7)
@@ -314,12 +301,14 @@ namespace ConsoleApp1
                     }
                 }
 
-                //дополнительные преобразования, 9ый раунд алгоритма
-                A = multiplication(A, subKeysDeshifr[48]);
-                B = addition(B, subKeysDeshifr[49]);
-                C = addition(C, subKeysDeshifr[50]);
-                D = multiplication(D, subKeysDeshifr[51]);
+                //дополнительные преобразования, 9ый раунд 
+                A = moduleOperations.multiplication(A, subKeysDeshifr[48]);
+                B = moduleOperations.addition(B, subKeysDeshifr[49]);
+                C = moduleOperations.addition(C, subKeysDeshifr[50]);
+                D = moduleOperations.multiplication(D, subKeysDeshifr[51]);
 
+
+                //
                 byte[] result = new byte[8];
                 result[0] = Convert.ToByte((A & 0xFF00) >> 8);
                 result[1] = Convert.ToByte(A & 0x00FF);
@@ -333,6 +322,7 @@ namespace ConsoleApp1
                 FSWrite.Write(result, 0, 8);
             }
 
+            //манипуляции с файлами чтобы "удалить" лишнее
             FileStream FSWrite1 = new FileStream(@".\decode.txt", FileMode.Create);
             int n = Convert.ToInt32(fstream.Length - 1 - countDeletByte); 
             byte[] arr = new byte[n];
@@ -346,10 +336,9 @@ namespace ConsoleApp1
             fstream.Close();
             
         }
-   
-    
+
         static void Main(string[] args)
-        {
+        {            
             generationKeys();
             generationDeKeys();
             encryptingIDEA();
